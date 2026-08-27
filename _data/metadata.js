005d6f647962ebc79c2d58113f9f8b508e8d228f
Sumit Prasad <80179704+sumitkp11@users.noreply.github.com>
2026-08-27 17:29:00 +0530

    Update blog title and author email in metadata

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 export default {
-	title: "The Sumit's AI Blog",
+	title: "Sumit's AI & Tech Notes",
 	url: "https://ai.sumitprasad.com/",
 	language: "en",
 	description: "A beginner’s journey into AI—sharing what I learn, what confuses me, and how I grow along the way.",
 	author: {
 		name: "Sumit Prasad",
-		email: "me@sumitkp.in",
+		email: "me@sumitprasad.com",
 		url: "https://sumitprasad.com/about/"
 	}
 }
